84e983f4acbcba4e8877d2577c07e7e476dd7600
Andy <the.andy.franklin@gmail.com>
2023-08-03 09:24:13 -0700

    login/signup modal working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,12 @@ import { posts } from "../stubs/posts";
 import Trending from "../components/trending";
 import ContentCreationBar from "../components/content-creation-bar";
 import Navbar from "../components/navbar";
+import Header from "../components/header";
 
 const App = () => {
   return (
-    <div className="bg-gray-800 text-white flex flex-col h-screen">
-      <header className="p-4 flex justify-between items-center border-b border-black">
-        <h1 className="text-2xl font-bold">Scrollr</h1>
-        <div className="flex items-center gap-4">
-          <button className="bg-white text-blue-500 px-4 py-2 rounded active:bg-gray-200">
-            Login
-          </button>
-          <button className="bg-white text-blue-500 px-4 py-2 rounded active:bg-gray-200">
-            Sign up
-          </button>
-        </div>
-      </header>
+    <div id="app" className="bg-gray-800 text-white flex flex-col h-screen">
+      <Header />
       <div className="flex h-0 flex-grow mx-4">
         <Navbar />
         <div className="flex flex-col flex-grow gap-4 p-4 overflow-auto">
